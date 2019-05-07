2b5a1261b6fe54b9f3df9798f9058d71037914c4
Bryan Naegele <bryannaegele@users.noreply.github.com>
2019-05-07 11:03:07 -0600

    Set content type header (#5)
    Added content-type header as text/plain to fix DD agents

--- test/router_test.exs
+++ test/router_test.exs
@@ -17,4 +17,21 @@ defmodule TelemetryMetricsPrometheus.RouterTest do
     assert conn.state == :sent
     assert conn.status == 404
   end
+
+  test "returns a scrape" do
+    # Create a test connection
+    conn = conn(:get, "/metrics")
+
+    TelemetryMetricsPrometheus.init([], [name: :test, port: 9999])
+
+    # Invoke the plug
+    conn = Router.call(conn, Router.init(name: :test))
+
+    # Assert the response and status
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert get_resp_header(conn, "content-type") |> hd() =~ "text/plain"
+
+    TelemetryMetricsPrometheus.stop(:test)
+  end
 end
