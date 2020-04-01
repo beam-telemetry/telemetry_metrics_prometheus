916e54f56101db5db6bd58a0ba1a17fe55801f02
Bryan Naegele <bryan@fourthtime.com>
2020-04-01 09:16:29 -0600

    Fix support for running multiple instances

--- lib/telemetry_metrics_prometheus/supervisor.ex
+++ lib/telemetry_metrics_prometheus/supervisor.ex
@@ -5,17 +5,18 @@ defmodule TelemetryMetricsPrometheus.Supervisor do
   alias TelemetryMetricsPrometheus.Router
 
   def start_link(init_args) do
-    Supervisor.start_link(__MODULE__, init_args, name: __MODULE__)
+    Supervisor.start_link(__MODULE__, init_args, name: :"#{init_args[:name]}_sup")
   end
 
   @impl true
   def init(args) do
     children = [
       {TelemetryMetricsPrometheus.Core, args},
-      {Plug.Cowboy,
-       scheme: Keyword.get(args, :protocol),
-       plug: {Router, [name: Keyword.get(args, :name)]},
-       options: Keyword.get(args, :options)}
+      Plug.Cowboy.child_spec(
+        scheme: Keyword.get(args, :protocol),
+        plug: {Router, [name: Keyword.get(args, :name)]},
+        options: Keyword.get(args, :options)
+      )
     ]
 
     Supervisor.init(children, strategy: :rest_for_one)
